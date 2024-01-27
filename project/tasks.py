e53e5be4ced34cb9b8b51cd5a2c5332cfb1239ec
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2024-01-27 23:16:13 +0200

    Fix html coverage report

--- project/tasks.py
+++ project/tasks.py
@@ -11,7 +11,7 @@ def test(ctx):
 @task    
 def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src",pty=True)
-    ctx.run("coverage report html")
+    ctx.run("coverage html")
 @task
 def format(ctx):
     ctx.run("autopep8 --in-place --recursive src", pty=True)
