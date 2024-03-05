0a4fea0e20e6674f9db9ea812aed42793562ccc2
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2024-03-05 18:02:09 +0200

    Write test for minimax

---
+++ src/tests/minimax_test.py
@@ -0,0 +1,20 @@
+import unittest
+from minimax import Minimax
+from connect4 import Connect4
+
+class TestMinimax(unittest.TestCase):
+	def setUp(self) -> None:
+		self.game = Connect4()
+		self.algo = Minimax()
+
+	def test_returns_winning_move_if_one_off_win(self):
+		self.game.table = [[0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0],
+                           [2, 2, 2, 0, 0, 1, 0]]
+		self.game.last_added = (5, 0)
+		score, move = self.algo._minimax(self.game, 3, True, -100000000000, 100000000000)
+		self.assertEqual(move, 3)
+		self.assertEqual(score, 10000)
