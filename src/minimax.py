9691f737508d22666ecc229b9010ddd25ebc8ebf
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2024-03-07 11:40:01 +0200

    Added sorting moves based on previous results to increase pruning

--- src/minimax.py
+++ src/minimax.py
@@ -17,10 +17,14 @@ class Minimax():
         if self.debug:
             self.result = []
         self.time_deepening = False
+        self._stored_results = {}
 
-    def _calculate_heuristic_score(self, game: Connect4, depth: int) -> int:
-        """Heuristic function that calls the heuristic class to calculate a heuristic value"""
-        return self.heuristic.calculate_score(game, depth)
+    def _store_result(self, game: Connect4, score: int):
+        key = ""
+        for row in game.table:
+            for item in row:
+                key += str(item)
+        self._stored_results[key] = score
 
     def _get_stored_result(self, game: Connect4):
         key = ""
@@ -28,12 +32,16 @@ class Minimax():
             for item in row:
                 key += str(item)
         if key in self._stored_results:
-            self._saved_calculations += 1
             return self._stored_results[key]
         return False
+    
+    def _calculate_heuristic_score(self, game: Connect4, depth: int) -> int:
+        """Heuristic function that calls the heuristic class to calculate a heuristic value"""
+        return self.heuristic.calculate_score(game, depth)
 
-    def _organise_moves(self, moves_length):
+    def _organise_base_moves(self, game:Connect4):
         new_moves_list = []
+        moves_length = len(game.table[0])
         index = floor(moves_length/2)
         down = True
         jump = 1
@@ -47,6 +55,29 @@ class Minimax():
                 down = True
             jump += 1
         return new_moves_list
+    
+    def _order_next_moves(self, game:Connect4, moves: list, maximising: bool):
+        if maximising:
+            player = 2
+        else:
+            player = 1
+        stored_results_next_moves = []
+        for move in moves:
+            new_game = Connect4()
+            new_game.table = deepcopy(game.table)
+            new_game.play_move(move, player)
+            stored_result = self._get_stored_result(new_game)
+            if stored_result is not False:
+                stored_results_next_moves.append((stored_result, move))
+        moves_copy = moves.copy()
+        moves_order_to_return = []
+        sorted_stored_results = sorted(stored_results_next_moves, key=lambda x: x[0])
+        for result in sorted_stored_results:
+            moves_order_to_return.append(result[1])
+            moves_copy.remove(result[1])
+        for move in moves_copy:
+            moves_order_to_return.append(move)
+        return moves_order_to_return
 
     def _time_check(self):
         if not self.time_deepening:
@@ -64,12 +95,13 @@ class Minimax():
         if self._time_check():
             return -1
         if first_run:
-            self.organised_moves = self._organise_moves(len(game.table[0]))
+            self.organised_moves = self._organise_base_moves(game)
         if depth == 0 or game.calculate_winner():
             return self._calculate_heuristic_score(game, depth), move
         if maximising:
             value = -100000000000
-            for index in self.organised_moves:
+            organised_next_moves = self._order_next_moves(game, self.organised_moves, maximising)
+            for index in organised_next_moves:
                 new_game = Connect4()
                 new_game.table = deepcopy(game.table)
                 if new_game.play_move(index, 2):
@@ -77,6 +109,7 @@ class Minimax():
                         move = index
                     result = self._minimax(
                         new_game, depth-1, False, alpha, beta, False, move)
+                    self._store_result(new_game, result[0])
                     if result == -1:
                         break
                     if first_run and self.debug:
@@ -90,7 +123,8 @@ class Minimax():
             return value, best_move if first_run else move
         else:
             value = 100000000000
-            for index in self.organised_moves:
+            organised_next_moves = self._order_next_moves(game, self.organised_moves, maximising)
+            for index in organised_next_moves:
                 new_game = Connect4()
                 new_game.table = deepcopy(game.table)
                 if new_game.play_move(index, 1):
@@ -98,6 +132,7 @@ class Minimax():
                         move = index
                     result = self._minimax(
                         new_game, depth-1, True, alpha, beta, False, move)
+                    self._store_result(new_game, result[0])
                     if result == -1:
                         break
                     if result[0] < value:
